a01bd1f2bf21af0402c835df938f8c3da5d54aeb
Vishal R <vishalramesh1721@gmail.com>
2026-03-16 21:15:29 +0530

    Update sensorDataAPI.js

--- frontend/src/APIs/sensorDataAPI.js
+++ frontend/src/APIs/sensorDataAPI.js
@@ -3,7 +3,7 @@ import { getToken } from "@/utils/auth";
 
 // Create axios instance
 const api = axios.create({
-  baseURL: "https://iot-dashboard-v5ab.onrender.com",
+  baseURL: "https://iot-dashboard-v5ab.onrender.com/api",
 });
 
 // ==============================
