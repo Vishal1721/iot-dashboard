2e1c2273b6033ab4b1e2476bfa9fcfdb1b1eb889
Vishal R <vishalramesh1721@gmail.com>
2026-03-16 21:13:16 +0530

    Update socket.js

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_SERVER_URL = "http://localhost:3000"; // or from env
+const SOCKET_SERVER_URL = "https://iot-dashboard-v5ab.onrender.com"; // or from env
 
 const socket = io(SOCKET_SERVER_URL, {
   autoConnect: true,
