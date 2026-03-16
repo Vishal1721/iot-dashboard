3db9a44e48e6138c6605ca2c7ffca3d5c93be3b0
Vishal R <vishalramesh1721@gmail.com>
2026-03-16 21:48:00 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "https://iot-dashboard-v5ab.onrender.com/api",
+  baseURL: "https://iot-dashboard-v5ab.onrender.com",
 });
 
 api.interceptors.request.use(
