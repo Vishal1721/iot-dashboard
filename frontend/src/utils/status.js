b4f9a93ceeb6f5712ad5141f7a6fb128d80882da
Vishal R <vishalramesh1721@gmail.com>
2026-03-16 21:50:20 +0530

    Update status.js

--- frontend/src/utils/status.js
+++ frontend/src/utils/status.js
@@ -2,7 +2,7 @@ import api from "./api";
 
 export const getStatus = async (userId) => {
   try {
-    const response = await api.get(`/users/profile`, { id: userId });
+    const response = await api.get(`/api/users/profile`, { id: userId });
     // console.log(response.data)
     if(response?.data?.status === 'success') return 'Connected'
     else if(response?.data?.data === 'Invalid token') return 'Expired'
