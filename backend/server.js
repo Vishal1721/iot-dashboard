ecf210252f53e9c6aa33ac00c850c6a5581c59d7
Vishal R <vishalramesh1721@gmail.com>
2026-04-26 16:00:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,14 @@ const io = new Server(server, {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://iot-dashboard-orpin.vercel.app"
+    ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+    credentials: true
+  })
 );
 app.use(express.json());
 app.get("/", (req, res) => {
